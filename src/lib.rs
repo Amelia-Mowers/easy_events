3b579ba556fd2154b2ece96bfd1bcc066cba145e
Amelia-Mowers <Amelia.Mowers@Outlook.com>
2023-02-19 21:20:20 -0800

    Added event id methods to event trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,36 @@ use std::any::Any;
 
 pub trait Event{
     fn as_any(&self) -> &dyn Any;
+    fn event_id() -> String;
+    fn event_id_this(&self) -> String;
 }
 
 #[macro_export]
 macro_rules! implement_event {
     (
-        $struct:ident
+        $struct:ident,
+        $event_id:expr
     ) => {
         impl Event for $struct {
             fn as_any(&self) -> &dyn Any{ self }
+            fn event_id() -> String{ String::from($event_id) }
+            fn event_id_this(&self) -> String{ String::from($event_id) }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    struct TestEvent;
+    use super::*;
+
+    implement_event!(TestEvent, "test_event");
+
+    #[test]
+    fn id_test() {
+        println!("{}", TestEvent::event_id());
+
+        let ev = TestEvent{};
+        println!("{}", ev.event_id_this());
+    }
 }
